fd352e01d3afbf9c3b04539af3b24df41091e037
Elena-2301 <elena.pehnova@gmail.com>
2024-10-08 15:59:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print(12 + 89 - 1)
+#"1st program".
+print(9 ** 0.5 * 5)
+#"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+#"3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+#"4th program"
+print('123.456')
+print(type('123.456'))
+print(123.456)
+print(type(123.456))
+print(123.456 * 10) # 1 действие
+print(int(1234.56 % 10)) # 2 действие
+
+print(int(float('123.456') * 10) % 10)
